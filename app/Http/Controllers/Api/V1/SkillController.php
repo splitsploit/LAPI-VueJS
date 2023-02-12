f2ea538110c6581aa5723a64685bf8473ec493fb
unknown <bayuxbca@gmail.com>
2023-02-12 09:48:33 +0700

    create,store,update Skill API

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Models\Skill;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreSkillRequest;
-use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
@@ -15,6 +16,20 @@ class SkillController extends Controller
 
     public function store(StoreSkillRequest $request)
     {
-        Skill::create($request->validated())
+        Skill::create($request->validated());
+        
+        return response()->json('Skill Created!');
+    }
+
+    public function update(StoreSkillRequest $request, Skill $skill)
+    {
+        // $data = $request->all();
+
+        // $skill = Skill::create($data);
+
+        $skill->update($request->validated());
+
+        return response()->json('Skill Updated!');
+
     }
 }
