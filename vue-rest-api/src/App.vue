6d41701af7e4459f88bdb00ce859aeef974c65d1
unknown <bayuxbca@gmail.com>
2023-02-13 21:36:54 +0700

    create composables with async await

--- vue-rest-api/src/App.vue
+++ vue-rest-api/src/App.vue
@@ -4,12 +4,15 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
+    <div class="max-w-5xl mx-auto">
+      <nav class="p-2">
+        <RouterLink to="/" class="px-4 py-2 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white">Home</RouterLink>
+        <RouterLink to="/skills" class="px-4 py-2 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white">Skills</RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+  <main class="max-w-7xl mx-auto min-h-screen">
+    <RouterView />
+  </main>
 </template>
--- vue-rest-api/src/router/index.js
+++ vue-rest-api/src/router/index.js
@@ -10,14 +10,21 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/HomeView.vue')
+      path: '/skills',
+      name: 'SkillIndex',
+      component: () => import('../views/skills/SkillIndex.vue')
+    },
+    {
+      path: '/skills/create',
+      name: 'SkillCreate',
+      component: () => import('../views/skills/SkillCreate.vue')
+    },
+    {
+      path: '/skills/:id/edit',
+      name: 'SkillSEdit',
+      component: () => import('../views/skills/SkillEdit.vue')
     }
   ]
 })
 
-export default router
+export default router;
